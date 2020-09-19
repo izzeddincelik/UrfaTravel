79f5729ef0971af9c9cba0dd7044e5b049f1b52d
izzeddincelik <izzeddinmupe@gmail.com>
2020-09-20 01:15:31 +0300

    urfa Travel Gobeklitepe

---
+++ app/src/main/java/com/urfatravel/urfatravelgobeklitepe/MainActivity.java
@@ -0,0 +1,29 @@
+package com.urfatravel.urfatravelgobeklitepe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webb = (WebView)findViewById(R.id.web1);
+        webb.setWebViewClient(new WebViewClient());
+        webb.loadUrl("https://urfatravel.com");
+
+        WebSettings webSettings = webb.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        //setContentView(R.layout.activity_main);
+    }
+}
